9536ae8c643617a40fb32c26ac0678acc188d38f
Fikri Ramadhan <ramadhan@kabayan.id>
2024-04-09 15:00:09 +0700

    fix sulaiman

--- resources/views/farmasi/cetak/resep.blade.php
+++ resources/views/farmasi/cetak/resep.blade.php
@@ -24,14 +24,16 @@
         <div class="row">
             <table>
                 <tr>
-                    <td rowspan="3"><img width="40" src="data:image/png;base64, {!! base64_encode(file_get_contents(public_path('image/logosiswanto.png'))) !!} "></td>
-                    <td>FARMASI RSAU dr.SISWANTO</td>
+                    <td rowspan="3">
+                        <img width="40" src="data:image/png;base64, {!! base64_encode(file_get_contents(public_path('image/LOGO_RUMKIT_SULAIMAN__2_-removebg-preview.png'))) !!} ">
+                    </td>
+                    <td>FARMASI RSAU dr. Norman T Lubis</td>
                 </tr>
                 <tr>
-                    <td><i>JL Tentara Pelajar No 1, Malangjiwan, Colomadu</i></td>
+                    <td><i>Jl. Terusan Kopo-Soreang No.461, Lanud Sulaiman</i></td>
                 </tr>
                 <tr>
-                    <td><i>Telepon 0271779112</i></td>
+                    <td><i></i></td>
                 </tr>
             </table>
         </div>
