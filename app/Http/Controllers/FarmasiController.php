cc1e78bac2488e35950aa3baeb7115f9876a87e7
Fikri Ramadhan <ramadhan@kabayan.id>
2024-05-08 05:54:27 +0700

    commit

--- app/Http/Controllers/FarmasiController.php
+++ app/Http/Controllers/FarmasiController.php
@@ -45,6 +45,7 @@ class FarmasiController extends Controller
         
     //    return $request->idantrian;
         // return $select_resep;
+        VclaimHelper::update_task($rawat->idrawat,7,$current_time);
         $obat_non_racik = 0;
         $obat_racikan = 0;
         $hitung_kronis = 0;
@@ -256,6 +257,7 @@ class FarmasiController extends Controller
     public function post_resep(Request $request, $id)
     {
         // return $request->all();
+        VclaimHelper::update_task($rawat->idrawat,6,$current_time);
         $antrian = AntrianFarmasi::find($id);
         if($antrian->update != 1){
             return back()->with('gagal', 'Pastikan data sudah di update');
@@ -461,6 +463,7 @@ class FarmasiController extends Controller
 
     public function status_rajal($id)
     {
+        VclaimHelper::update_task($rawat->idrawat,6,$current_time);
         $rawat = Rawat::find($id);
         $pasien = Pasien::where('no_rm', $rawat->no_rm)->first();
         $antrian = AntrianFarmasi::where('idrawat', $id)->where('status_antrian','Antrian')->first();
