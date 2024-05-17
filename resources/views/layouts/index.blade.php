289b662089c4a831b14a19194269fee49ba97278
Fikri Ramadhan <ramadhan@kabayan.id>
2024-05-17 14:09:22 +0700

    fix blockui

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -31,7 +31,7 @@
     data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true"
     data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true"
     data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default">
-    <!--begin::App-->
+    
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
         <!--begin::Page-->
         <div class="app-page flex-column flex-column-fluid" id="kt_app_page">
